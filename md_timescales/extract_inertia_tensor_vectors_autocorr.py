88ba247e36eca0884d089e1aa1d20843070ce3e4
legosta <stanislav.legkovoy@gmail.com>
2019-03-24 16:28:24 +0300

    delete tabs and correct an error with parser name(trajectory)

--- md_timescales/extract_inertia_tensor_vectors_autocorr.py
+++ md_timescales/extract_inertia_tensor_vectors_autocorr.py
@@ -29,14 +29,14 @@ def extract_inertia_tensor_vectors_autocorr(path_to_trajectory: str, output_dire
         if frame_ca is None:
             frame_ca = frame.asAtoms.filter(aName == "CA")
 
-    al = ref_ca.alignment_to(frame_ca)
-    m = al.matrix3d()
-    v1 = m.dot(eigen_vectors[0])
-    vectors1.append(XYZ(v1[0], v1[1], v1[2]))
-    v2 = m.dot(eigen_vectors[1])
-    vectors2.append(XYZ(v2[0], v2[1], v2[2]))
-    v3 = m.dot(eigen_vectors[2])
-    vectors3.append(XYZ(v3[0], v3[1], v3[2]))
+        al = ref_ca.alignment_to(frame_ca)
+        m = al.matrix3d()
+        v1 = m.dot(eigen_vectors[0])
+        vectors1.append(XYZ(v1[0], v1[1], v1[2]))
+        v2 = m.dot(eigen_vectors[1])
+        vectors2.append(XYZ(v2[0], v2[1], v2[2]))
+        v3 = m.dot(eigen_vectors[2])
+        vectors3.append(XYZ(v3[0], v3[1], v3[2]))
 
     autocorr1 = calc_autocorr_order_2(vectors1)
     autocorr2 = calc_autocorr_order_2(vectors2)
@@ -52,7 +52,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Calculate_overall_tumbling')
     parser.add_argument('-i', '--path_to_trajectory', required=True, )
     parser.add_argument('-o', '--output_directory', default=os.getcwd())
-    parser.add_argument('-l', '--length_trajectory', default=1, type=int)
+    parser.add_argument('-l', '--trajectory_length', default=1, type=int)
     args = parser.parse_args()
     extract_inertia_tensor_vectors_autocorr(args.path_to_trajectory, args.output_directory, args.trajectory_length)
 
