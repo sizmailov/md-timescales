700a48c31a10a315351cd85bd169918d39c87f1b
OOLebedenko <oolebedenko@gmail.com>
2019-03-26 01:25:16 +0300

    Add with PdfPages indtead open

--- md_timescales/plot_NH_autocorr.py
+++ md_timescales/plot_NH_autocorr.py
@@ -33,51 +33,50 @@ def plot_acorr_fit(path_to_fit_csv, path_to_accor_csv, path_to_output_pdf):
     exp_order = {2: "tau_NH_2_exp", 3: "tau_NH_3_exp", 4: "tau_NH_4_exp"}
     csv_files = sorted(glob.glob(os.path.join(path_to_accor_csv, "*.csv")))
     for order in range(2,5):
-        pdf = PdfPages(os.path.join(path_to_output_pdf, exp_order[order] + ".pdf"))
-        csv_fit =  os.path.join(path_to_fit_csv, exp_order[order] + ".csv")
-        fit = pd.read_csv(csv_fit)
-        for ind, file in enumerate(csv_files):
-            df = pd.read_csv(file)
-            fit_line = fit.iloc[ind]
-            amplitude_label = ["exp-%d-a%d" %  (order, i + 1) for i in range(order)]
-            tau_label = ["exp-%d-tau%d" %  (order, i + 1) for i in range(order)]
-            popt = {"a%d" % (i+1): fit_line[amplitude_label[i]] for i in range(order)}
-            tau = {"tau%d" % (i+1): fit_line[tau_label[i]] for i in range(order)}
-            popt.update(tau)
+        with PdfPages(os.path.join(path_to_output_pdf, exp_order[order] + ".pdf")) as pdf:
+            csv_fit =  os.path.join(path_to_fit_csv, exp_order[order] + ".csv")
+            fit = pd.read_csv(csv_fit)
+            for ind, file in enumerate(csv_files):
+                df = pd.read_csv(file)
+                fit_line = fit.iloc[ind]
+                amplitude_label = ["exp-%d-a%d" %  (order, i + 1) for i in range(order)]
+                tau_label = ["exp-%d-tau%d" %  (order, i + 1) for i in range(order)]
+                popt = {"a%d" % (i+1): fit_line[amplitude_label[i]] for i in range(order)}
+                tau = {"tau%d" % (i+1): fit_line[tau_label[i]] for i in range(order)}
+                popt.update(tau)
 
-            coeff_a = ["a%d" % (i+1) for i in range(order)]
-            coeff_tau = ["tau%d" % (i+1) for i in range(order)]
-            union_a_tau = list(zip(coeff_a,
-                           [" = " + str(round(popt[label],2)) + " " for label in coeff_a],
-                           coeff_tau,
-                           [" = " + str(round(popt[label],2)) + " ns\n" for label in coeff_tau]))
-            graph_label = "".join(["".join(elem) for elem in union_a_tau])
-            left, width = .40, .54
-            bottom, height = .40, .54
-            right = left + width
-            top = bottom + height
+                coeff_a = ["a%d" % (i+1) for i in range(order)]
+                coeff_tau = ["tau%d" % (i+1) for i in range(order)]
+                union_a_tau = list(zip(coeff_a,
+                               [" = " + str(round(popt[label],2)) + " " for label in coeff_a],
+                               coeff_tau,
+                               [" = " + str(round(popt[label],2)) + " ns\n" for label in coeff_tau]))
+                graph_label = "".join(["".join(elem) for elem in union_a_tau])
+                left, width = .40, .54
+                bottom, height = .40, .54
+                right = left + width
+                top = bottom + height
 
-            fig = plt.figure()
-            ax = fig.add_subplot(111)
-            ax.text(right, top, graph_label,
-                horizontalalignment='right',
-                verticalalignment='top',
-                transform=ax.transAxes,
-                multialignment='left',
-                bbox={'facecolor': 'moccasin', 'alpha': 0.5, 'pad': 6})
-            ax.set_ylim(-0.1,1.1)
-            ax.set_xlim(-1, 20)
-            ax.set_xlabel('time, ns', fontsize = 13)
-            ax.set_ylabel('autocorrelation', fontsize = 13)
-            ax.set_title('NH autocorrelation plot %s exp %s%s'%(str(order), fit_line["rId"], fit_line["rName"]))
-            ax.plot( df.time_ns, df.acorr)
-            ax.plot(df.time_ns, fit_func[order](df.time_ns, **popt))
-            #ax.set_axvline(x=df.time_ns[limit], color='g', linestyle='--', label="fit limit %s"%(limit))
-            ax.grid(True)
-            # ax.set_legend(("NH autocorrelation", "fit , loc='upper right')
-            pdf.savefig()
-            plt.close()
-        pdf.close()
+                fig = plt.figure()
+                ax = fig.add_subplot(111)
+                ax.text(right, top, graph_label,
+                    horizontalalignment='right',
+                    verticalalignment='top',
+                    transform=ax.transAxes,
+                    multialignment='left',
+                    bbox={'facecolor': 'moccasin', 'alpha': 0.5, 'pad': 6})
+                ax.set_ylim(-0.1,1.1)
+                ax.set_xlim(-1, 20)
+                ax.set_xlabel('time, ns', fontsize = 13)
+                ax.set_ylabel('autocorrelation', fontsize = 13)
+                ax.set_title('NH autocorrelation plot %s exp %s%s'%(str(order), fit_line["rId"], fit_line["rName"]))
+                ax.plot( df.time_ns, df.acorr)
+                ax.plot(df.time_ns, fit_func[order](df.time_ns, **popt))
+                #ax.set_axvline(x=df.time_ns[limit], color='g', linestyle='--', label="fit limit %s"%(limit))
+                ax.grid(True)
+                # ax.set_legend(("NH autocorrelation", "fit , loc='upper right')
+                pdf.savefig()
+                plt.close()
 
 if __name__ == "__main__":
     #-f_csv "/home/olebedenko/bioinf/handling/h4/tip4p-ew/NPT_gamma_ln_2/autocorr/NH/graph" -a_csv "/home/olebedenko/bioinf/handling/h4/tip4p-ew/NPT_gamma_ln_2/autocorr/NH/data"
