0e6dee8adf1b93165e14549b8888f76b7baa2746
OOLebedenko <oolebedenko@gmail.com>
2019-03-28 12:55:25 +0300

    Iitial makefile_NH_acorr

---
+++ analysis_template/autocorr/NH/common.mk
@@ -0,0 +1,12 @@
+SCRIPT_DIR=/home/olebedenko/bioinf/md-timescales/md_timescales/
+N_RESIDUES=76
+TRAJECTORY_LENGTH=1000
+TRAJECTORY_PATH=/home/olebedenko/bioinf/trj/ubq/spce/NVE/
+
+.PHONY: all clean
+
+all: graph/tau_NH_2_exp.pdf graph/tau_NH_4_exp.pdf
+
+clean:
+	rm -rf data 
+	rm -rf graph
