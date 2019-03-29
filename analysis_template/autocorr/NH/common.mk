d149ad010d7b742638171827b4797649316fd0f1
OOLebedenko <oolebedenko@gmail.com>
2019-03-29 19:42:16 +0300

    change layout to data/fit data/acorr figures/

--- analysis_template/autocorr/NH/common.mk
+++ analysis_template/autocorr/NH/common.mk
@@ -1,12 +1,12 @@
 SCRIPT_DIR=/home/olebedenko/bioinf/scripts/md-timescales/md_timescales/
 N_RESIDUES=76
-TRAJECTORY_LENGTH=1000
+TRAJECTORY_LENGTH=10
 TRAJECTORY_PATH=/home/olebedenko/bioinf/trj/ubq/spce/NVE/
 
 .PHONY: all clean
 
-all: graph/tau_NH_2_exp.pdf graph/tau_NH_4_exp.pdf
+all: figures/tau_NH_2_exp.pdf figures/tau_NH_4_exp.pdf
 
 clean:
 	rm -rf data 
-	rm -rf graph
+	rm -rf figures
