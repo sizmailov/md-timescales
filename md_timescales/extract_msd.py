b15405e70438193b3f81238565c906dd69ca9c1e
OOLebedenko <oolebedenko@gmail.com>
2019-04-17 17:56:32 +0300

    thin data

--- md_timescales/extract_msd.py
+++ md_timescales/extract_msd.py
@@ -1,4 +1,4 @@
-from bionmr_utils.md import  *
+from bionmr_utils.md import *
 from tqdm import tqdm
 import argparse
 import numpy as np
@@ -6,17 +6,26 @@ import os
 import pandas as pd
 
 
+def decrease_points_in_data(array):
+    # end_point = int(np.log2(len(array)))
+    # index = np.logspace(0, end_point, num=end_point + 1, endpoint=True, base=2)
+    # new_array = np.array([array[int(j)] for j in index])
+    new_array = np.array([array[i] for i in range(0,len(array), 10)])
+    return new_array
+
+
 def get_msd(array):
-    shifts = np.arange(len(array))
+    new_array = decrease_points_in_data(array)
+    shifts = np.arange(len(new_array))
     msd = np.zeros(shifts.size)
-    for i, shift in enumerate(shifts):
-        diffs = array[:-shift if shift else None] - array[shift:]
+    for i, shift in enumerate(tqdm(shifts)):
+        diffs = new_array[:-shift if shift else None] - new_array[shift:]
         sqdist = np.square(diffs).sum(axis=1)
         msd[i] = sqdist.mean()
     return msd
 
 
-def extract_mass_center_without_shift(path_to_trajectory: str, trajectory_length: int=1):
+def extract_center_mass_from_trajectory(path_to_trajectory: str, trajectory_length: int = 1):
     """
     :param path_to_trajectory:
     :param trajectory_length: by default equal 1
@@ -24,33 +33,35 @@ def extract_mass_center_without_shift(path_to_trajectory: str, trajectory_length
     atomics_weight = {'H': 1.008, 'C': 12.011, 'N': 14.007, 'O': 15.999, 'P': 30.973762, 'S': 32.06}
     traj, ref = traj_from_dir(path_to_trajectory, first=1, last=trajectory_length)
     masses = [atomics_weight[atom.name.str[0]] for atom in ref.asAtoms]
-    center_all_without_shift = []
-    for frame in  tqdm(traj):
+    center_all_without_shift = VectorXYZ()
+    for frame in traj:
         atoms = frame.asAtoms
         center = atoms.mass_center(masses)
         center_all_without_shift.append(center)
     return center_all_without_shift
 
 
-def extract_mass_center_with_shift(path_to_trajectory: str, output_directory: str, volume=None):
-    lattice_path = os.path.join(path_to_trajectory,"1_build", "box.inpcrd")
+def extract_mass_center_with_shift(path_to_trajectory: str, trajectory_length: int, volume=None):
+    lattice_path = os.path.join(path_to_trajectory, "1_build", "box.inpcrd")
     with open(lattice_path, "r") as in_file:
         last_line = in_file.readlines()[-1].split()
         vectors = [float(coordinate) for coordinate in last_line[0:3]]
         angles = [Degrees(float(coordinate)) for coordinate in last_line[3:]]
-        print(angles)
     latice = LatticeVectors(vectors[0], vectors[1], vectors[2],
                             angles[0], angles[1], angles[2])
     if volume:
-        volume_0 = (latice[0].cross(latice[1]), axisc=0).dot(latice[2])
+        volume_0 = (latice[0].cross(latice[1])).dot(latice[2])
         factor_scale = (float(volume[0]) / float(volume_0)) ** (1 / 3)
         latice.scale_by(factor_scale)
     else:
         basis = BestShiftFinder(latice)
+
     center_all_with_shift = pd.DataFrame()
-    center_all_without_shift = extract_mass_center_without_shift(path_to_trajectory, output_directory)
+    center_all_without_shift = extract_center_mass_from_trajectory(path_to_trajectory, trajectory_length)
     prev_center = None
-    for ind, center in enumerate(center_all_without_shift):
+    center_all_with_shift = VectorXYZ()
+
+    for ind, center in tqdm(enumerate(center_all_without_shift)):
         if prev_center is None:
             prev_center = center_all_without_shift[ind]
         else:
@@ -58,23 +69,17 @@ def extract_mass_center_with_shift(path_to_trajectory: str, output_directory: st
                 factor_scale = (float(volume[ind]) / float(volume[ind - 1])) ** (1 / 3)
                 latice.scale_by(factor_scale)
                 basis = BestShiftFinder(latice)
-            temp = pd.DataFrame({'x': prev_center.x, 'y': prev_center.y, 'z': prev_center.z}, index=[0],
-                                columns=['x', 'y', 'z'])
-            center_all_with_shift = pd.concat([center_all_with_shift, temp])
+            center_all_with_shift.append(prev_center)
             current_center = center
             length, shift = basis.find_best_shift(prev_center, current_center)
             prev_center = current_center + shift
-
-    pd.DataFrame(np.array([np.linspace(0, len(center_all_with_shift) * 0.002, len(center_all_with_shift), endpoint=False),
-                 center_all_with_shift.x, center_all_with_shift.y, center_all_with_shift.z]).T,
-                 columns=["time_ns", "x", "y", "z"]).to_csv(os.path.join(output_directory, "center_mass_coordinates.csv"), index=False)
-
+    center_all_with_shift = center_all_with_shift.to_numpy()
     return center_all_with_shift
 
 
-def extract_msd(path_to_trajectory: str, output_directory: str, volume=None):
+def extract_msd(path_to_trajectory: str, output_directory: str, trajectory_length: int, volume=None):
     center_all_with_shift = extract_mass_center_with_shift(
-        path_to_trajectory, output_directory, volume = None)
+        path_to_trajectory, trajectory_length, volume=None)
 
     arr = np.array(center_all_with_shift)
     msd = get_msd(arr)
@@ -86,10 +91,10 @@ def extract_msd(path_to_trajectory: str, output_directory: str, volume=None):
 
 if __name__ == '__main__':
     # -i "/home/olebedenko/bioinf/1ubq/1ubq/"
-    parser = argparse.ArgumentParser(description='Calc NH autocorr')
-    parser.add_argument('-i', '--path_to_trajectory', required=True, )
-    parser.add_argument('-o', '--output_directory', default=os.getcwd())
-    parser.add_argument('-v', '--volume', default=False, bool=int)
+    parser = argparse.ArgumentParser(description='Mean square displacement centr mass (msd)')
+    parser.add_argument('--path-to-trajectory', required=True, )
+    parser.add_argument('--output-directory', default=os.getcwd())
+    parser.add_argument('--trajectory-length', required=True, type=int)
+    parser.add_argument('--volume', default=False)
     args = parser.parse_args()
-    extract_msd(args.path_to_trajectory, args.output_directory, args.volume)
-   
+    extract_msd(args.path_to_trajectory, args.output_directory, args.trajectory_length, args.volume)
--- md_timescales/plot_msd.py
+++ md_timescales/plot_msd.py
@@ -27,41 +27,40 @@ def linear_fit(x, a1):
     return a1*x
 
 
-def get_translation_fit(path_to_msd, limit, output_directory): 
+def get_translation_fit(path_to_msd, output_directory): 
     df_msd = pd.read_csv(path_to_msd)
-    popt, pcov = curve_fit(linear_fit, df_msd.time_ns[:limit], df_msd.delta_rsquare[:limit])
+    #popt, pcov = curve_fit(linear_fit, df_msd.time_ns[:limit], df_msd.delta_rsquare[:limit])
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    graph_label = "{a1:.2f}".format(a1=popt[0]) + " A^2/ns"
+    # graph_label = "{a1:.2f}".format(a1=popt[0]) + " A^2/ns"
     left, width = .08, .08
     bottom, height = .43, .43
     right = left + width
     top = bottom + height
-    ax.text(right, top, graph_label,
-        horizontalalignment='left',
-        verticalalignment='top',
-        transform=ax.transAxes,
-        multialignment='left',
-        bbox={'facecolor': 'moccasin', 'alpha': 0.5, 'pad': 6})
+    # ax.text(right, top, graph_label,
+    #     horizontalalignment='left',
+    #     verticalalignment='top',
+    #     transform=ax.transAxes,
+    #     multialignment='left',
+    #     bbox={'facecolor': 'moccasin', 'alpha': 0.5, 'pad': 6})
     ax.set_xlabel('time, ns', fontsize = 13)
     ax.set_ylabel('msd, A^2', fontsize = 13)
     ax.set_title('Mean square displacement center mass (msd)')
     ax.plot(df_msd.time_ns, df_msd.delta_rsquare)
-    ax.plot(df_msd.time_ns, linear_fit(df_msd.time_ns, *popt))
-    ax.axvline(x=df_msd.time_ns[limit], color='g', linestyle='--', label="fit limit %s"%(limit))
+    #ax.plot(df_msd.time_ns, linear_fit(df_msd.time_ns, *popt))
+    # ax.axvline(x=df_msd.time_ns[limit], color='g', linestyle='--', label="fit limit %s"%(limit))
     ax.grid(True)
     plt.savefig(os.path.join(output_directory, "fit_msd_plot.png"))
     plt.close()
-    return popt
+    # return popt
 
 
 
 if __name__ == '__main__':	
     # -i "/home/olebedenko/bioinf/scripts/md-timescales/md_timescales/msd.csv -lim 78000"
     parser = argparse.ArgumentParser(description='Plot msd autocorrelation')
-    parser.add_argument('-i', '--path_to_msd', required=True)
-    parser.add_argument('-lim', '--limit', type=int, required=True)
-    parser.add_argument('-o', '--output_directory', default=os.getcwd())
+    parser.add_argument('--path-to-msd', required=True)
+    parser.add_argument('--output-directory', default=os.getcwd())
     args = parser.parse_args()
-    get_translation_fit(args.path_to_msd, args.limit, args.output_directory)
+    get_translation_fit(args.path_to_msd, args.output_directory)
