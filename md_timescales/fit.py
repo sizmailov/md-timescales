cdaa4f3f21e852ba2ce70af8112ec6d0f23f9f75
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2019-04-23 12:53:33 +0300

    fix: restore bounds to avoid cumulative scaling

--- md_timescales/fit.py
+++ md_timescales/fit.py
@@ -112,7 +112,10 @@ def decorated_fit_auto_correlation(time: List[float],
                                     np.array(multi_exp(time, *popt))) ** 2))
             popt_all.append(popt)
         except RuntimeError:
-            print("Fit error n={}, scale={}".format(len(bounds)//2, scale))
+            print("Fit error n={}, scale={}".format(len(bounds[0])//2, scale))
+
+        scale_times(bounds[0], 1.0/scale)
+        scale_times(bounds[1], 1.0/scale)
 
     min_ind_r_square = np.argmin(R_square)
 
